abc8b9d7cccadfc570b95f2261040659360d3c54
n3ur0sec <146773365+n3ur0sec@users.noreply.github.com>
2024-09-10 23:44:39 +0300

    Add files via upload

---
+++ list_directories.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the directory to start the search
+dir="/vhost/wphost2"
+
+# File to store the output
+output_file="directory_list.txt"
+
+# Search for directories
+subdirs=$(find $dir -mindepth 1 -type d ! -path "*www*" ! -path "*members*" ! -path "*old*" ! -path "*harleystreetnurses.com*" ! -path "*old_members*" ! -path "*learningaboutdogs.com*" ! -path "*heads2gether.net*")
+
+# Loop through the subdirectories
+for subdir in $subdirs; do
+    # Print the current subdirectory to the output file
+    echo $subdir >> $output_file
+done
